40f4be0e96736a644c1062428374a48f9f79dbf2
Simsblock <146435639+Simsblock@users.noreply.github.com>
2026-05-26 18:12:55 +0200

    New misc. pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -43,6 +43,10 @@ export default defineConfig({
                     { label: 'Color Text', slug: 'miscellaneous/qol/color-text' },
                     { label: 'Custom Armor Stands', slug: 'miscellaneous/features/armor-stands' },
                     { label: 'Horse Stats', slug: 'miscellaneous/qol/horse-stats' },
+                    { label: 'Horse Stats', slug: 'miscellaneous/qol/ping/' },
+                    { label: 'Horse Stats', slug: 'miscellaneous/social/leash/' },
+                    { label: 'Horse Stats', slug: 'miscellaneous/social/kiss/' },
+                    { label: 'Horse Stats', slug: 'miscellaneous/social/feed/' },
                     { label: 'Voice Chat', slug: 'miscellaneous/social/voice-chat' },
                 ],
             }
