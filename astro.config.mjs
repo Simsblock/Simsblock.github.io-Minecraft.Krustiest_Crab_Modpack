1403a59357a70f6eab306f23b95def1ff040bd1b
Simsblock <146435639+Simsblock@users.noreply.github.com>
2026-05-26 13:13:36 +0200

    first pages + deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,25 +2,50 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
+import mdx from '@astrojs/mdx';
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					items: [{ autogenerate: { directory: 'reference' } }],
-				},
-			],
-		}),
-	],
+
+    site: 'https://Simsblock.github.io', 
+    base: '/Simsblock.github.io-Minecraft.Krustiest_Crab_Modpack',
+
+    integrations: [starlight({
+        title: 'Krustiest Crab',
+        social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+        sidebar: [
+            {
+                label: 'Progression',
+                items: [
+                    // Each item here is one entry in the navigation menu.
+                    { label: 'Example Guide', slug: 'guides/example' },
+                ],
+            },
+            {
+                label: 'Create',
+                items: [{ autogenerate: { directory: 'reference' } }],
+            },
+            {
+                label: 'Enchanting',
+                items: [
+                    // Each item here is one entry in the navigation menu.
+                    { label: 'Example Guide', slug: 'guides/example' },
+                ],
+            },
+            {
+                label: 'Advice',
+                items: [{ autogenerate: { directory: 'reference' } }],
+            },
+                        {
+                label: 'Miscellaneous',
+                items: [
+                    // Each item here is one entry in the navigation menu.
+                    { label: 'Color Text', slug: 'miscellaneous/qol/color-text' },
+                    { label: 'Custom Armor Stands', slug: 'miscellaneous/features/armor-stands' },
+                    { label: 'Horse Stats', slug: 'miscellaneous/qol/horse-stats' },
+                    { label: 'Voice Chat', slug: 'miscellaneous/social/voice-chat' },
+                ],
+            }
+        ],
+		}), mdx()],
 });
